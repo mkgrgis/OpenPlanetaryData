182c3b2f89ff16396369b06d7c92c79d0ee909d0
Михаил <41448637+mkgrgis@users.noreply.github.com>
2019-02-17 13:31:40 +0300

    Add files via upload

---
+++ OpenPlanetaryData USGS sync.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Creates in a current directory a copy of USGS planeterynames data
+d="$PWD/OpenPlanetaryData";
+mkdir "$d" 2>/dev/null;
+f="$d/universal.json";
+b="https://raw.githubusercontent.com/mkgrgis/OpenPlanetaryData/master";
+usgsb="http://planetarynames.wr.usgs.gov/shapefiles";
+wget -O - "$b/universal.json" 2> /dev/null > "$f";
+n=$(cat "$f"| jq ".systeme|length");
+dir=$(cat "$f"| jq ".systeme|keys");
+for ((i=0; i < $n; i++)) do
+  PlnInd=$(echo "$dir" | jq .[$i] | sed 's,",,g');
+  mkdir "$d/$PlnInd";
+done;
+
+for ((i=0; i < $n; i++)) do
+  PlnInd=$(echo "$dir" | jq .[$i] | sed 's,",,g');
+  planet=$(cat "$f"| jq ".systeme[\"$PlnInd\"]" | sed 's,",,g');
+  echo "$PlnInd $planet";
+  wget -O - "$usgsb/${planet}_nomenclature.zip" 2> /dev/null > "$d/$PlnInd/${planet}_nomenclature.zip";
+done;
+echo;
+n=$(cat "$f"| jq ".element|length");
+dir=$(cat "$f"| jq ".element|keys");
+for ((i=0; i < $n; i++)) do
+  planet=$(echo "$dir" | jq .[$i] | sed 's,",,g');
+  dirPln=$(cat "$f"| jq ".element[\"$planet\"]" | sed 's,",,g');
+  echo "$dirPln $planet";
+  wget -O - "$usgsb/${planet}_nomenclature.zip" 2> /dev/null > "$d/$dirPln/${planet}_nomenclature.zip";
+done;
+
